ed796cb2bdbda0418339f01db527f3901daa9af4
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2018-01-13 09:01:50 +0100

    Content disposition filename
    File name is extracted from headers.

--- src/display/api.js
+++ src/display/api.js
@@ -2011,8 +2011,10 @@ var WorkerTransport = (function WorkerTransportClosure() {
         return {
           info: results[0],
           metadata: (results[1] ? new Metadata(results[1]) : null),
+          contentDispositionFileName: (this._fullReader ?
+                                       this._fullReader.fileName : null),
         };
-      });
+      }.bind(this));
     },
 
     getStats: function WorkerTransport_getStats() {
